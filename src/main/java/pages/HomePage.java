20c4c4517c90a170c3274a8e6d264a6b56075a6d
alexander <pancasancha4@gmail.com>
2020-06-13 14:16:14 +0300

    new method for finding WebElements on HomePage -add formHomePage for init by PageFactory -child elements are searched inside the form

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -2,14 +2,17 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends BasePage {
+	@FindBy(xpath = "//div[@class='services services_main']")
+	WebElement formHomePage;
 	public void chooseDeposit(String optionName) {
 		waitClickable(optionLeftMenu(optionName)).click();
 	}
 
 	private WebElement optionLeftMenu(String option) {
-		WebElement optionLeft = findElement(By.xpath(String.format("//div[contains(text(),'%s')]/preceding-sibling::a", option)));
+		WebElement optionLeft = formHomePage.findElement(By.xpath(String.format("//div[contains(text(),'%s')]/preceding-sibling::a", option)));
 		return optionLeft;
 	}
 }
